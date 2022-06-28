1d5668f5eb3272632f4153996d3ceacc72deb67d
Naxthephantom <80404140+Naxthephantom@users.noreply.github.com>
2022-06-28 12:32:13 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,11 +1,16 @@
+
 from time import sleep
 from selenium import webdriver
 
-browser = webdriver.brave()
+browser = webdriver.chromedriver()
+browser.implicitly_wait(7)
 
 url = 'https:www.instagram.com/?hl=en'
 
 browser.get( url )
+
+login_link = browser.find_element_by_xpath("//a[text()='log in']")
+login_link.click()
 
 sleep(5)
 
