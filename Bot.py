e77a14ed4a73df6267355dd2ad5237919df88f07
Naxthephantom <80404140+Naxthephantom@users.noreply.github.com>
2022-07-02 20:57:56 +0200

    Create Bot.py

--- Bot.py
+++ Bot.py
@@ -1,12 +1,18 @@
 
+from lib2to3.pgen2 import driver
 from time import sleep
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-browser = webdriver.chromedriver()
+Options = Options()
+Options.binary_location = ( "C:\Users\Public\Desktop\Brave.lnk" )
+driver_path = ( "C:\Users\odiin\Documents\chrome driver\chromedriver.exe" )
+driver = webdriver.Chrome(options = Options, executable_path = driver_path)
+
+browser = webdriver.chrome()
 browser.implicitly_wait(7)
 
 url = 'https:www.instagram.com/?hl=en'
-
 browser.get( url )
 
 login_link = browser.find_element_by_xpath("//a[text()='log in']")
